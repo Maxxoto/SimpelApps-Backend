99a617067e075f206b77d5090fb3ec138a7538a9
Ubuntu <ubuntu@yokasoft.id>
2021-01-17 16:55:45 +0000

    Configuration for production

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    'mongodb://hamlet:toor@172.17.0.1:27017/simpelapps_dev?authSource=admin',
+  secret: 'simpelapps-secret',
+  baseURL: 'http://localhost:9000',
+};
